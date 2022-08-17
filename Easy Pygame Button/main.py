699f78d5393ebea25b60f185b597a309d9de399e
DionMitchell <99074044+DionMitchell@users.noreply.github.com>
2022-08-18 09:42:23 +1000

    Update main.py
    corrected positioning

--- Easy Pygame Button/main.py
+++ Easy Pygame Button/main.py
@@ -21,15 +21,15 @@ class Button:
 
 		self.over = False
 
-		if self.align == 'center': self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y-20), self.msg, white)
-		elif self.align == 'top': self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y), self.msg, white)
-		elif self.align == 'bottom': self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y-40), self.msg, white)
-		elif self.align == 'left': self.button = ft30.render_to(self.surf, (self.x, self.y-20), self.msg, white)
-		elif self.align == 'right': self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y-20), self.msg, white)
-		elif self.align == 'topleft': self.button = ft30.render_to(self.surf, (self.x, self.y), self.msg, white)
-		elif self.align == 'topright': self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y), self.msg, white)
-		elif self.align == 'bottomleft': self.button = ft30.render_to(self.surf, (self.x, self.y-40), self.msg, white)
-		elif self.align == 'bottomright': self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y-40), self.msg, white)
+		if self.align == 'center': self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y-21), self.msg, white)
+		elif self.align == 'top': self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y-1), self.msg, white)
+		elif self.align == 'bottom': self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y-41), self.msg, white)
+		elif self.align == 'left': self.button = ft30.render_to(self.surf, (self.x, self.y-21), self.msg, white)
+		elif self.align == 'right': self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y-21), self.msg, white)
+		elif self.align == 'topleft': self.button = ft30.render_to(self.surf, (self.x, self.y-1), self.msg, white)
+		elif self.align == 'topright': self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y-1), self.msg, white)
+		elif self.align == 'bottomleft': self.button = ft30.render_to(self.surf, (self.x, self.y-41), self.msg, white)
+		elif self.align == 'bottomright': self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y-41), self.msg, white)
 
 	def update(self):
 		if self.button.collidepoint(py.mouse.get_pos()): self.over = True
@@ -39,31 +39,31 @@ class Button:
 	def draw(self):
 		if self.align == 'center':
 			if self.over: self.button = ft34.render_to(self.surf, (self.x-10*len(self.msg), self.y-22.5), self.msg, white)
-			else: self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y-20), self.msg, white)
+			else: self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y-21), self.msg, white)
 		if self.align == 'top':
 			if self.over: self.button = ft34.render_to(self.surf, (self.x-10*len(self.msg), self.y), self.msg, white)
-			else: self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y), self.msg, white)
+			else: self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y-1), self.msg, white)
 		if self.align == 'bottom':
 			if self.over: self.button = ft34.render_to(self.surf, (self.x-10*len(self.msg), self.y-45), self.msg, white)
-			else: self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y-40), self.msg, white)
+			else: self.button = ft30.render_to(self.surf, (self.x-9*len(self.msg), self.y-41), self.msg, white)
 		if self.align == 'left':
 			if self.over: self.button = ft34.render_to(self.surf, (self.x, self.y-45), self.msg, white)
-			else: self.button = ft30.render_to(self.surf, (self.x, self.y-40), self.msg, white)
+			else: self.button = ft30.render_to(self.surf, (self.x, self.y-41), self.msg, white)
 		if self.align == 'right':
 			if self.over: self.button = ft34.render_to(self.surf, (self.x-20*len(self.msg), self.y-45), self.msg, white)
-			else: self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y-40), self.msg, white)
+			else: self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y-41), self.msg, white)
 		if self.align == 'topleft':
 			if self.over: self.button = ft34.render_to(self.surf, (self.x, self.y), self.msg, white)
-			else: self.button = ft30.render_to(self.surf, (self.x, self.y), self.msg, white)
+			else: self.button = ft30.render_to(self.surf, (self.x, self.y-1), self.msg, white)
 		if self.align == 'topright':
 			if self.over: self.button = ft34.render_to(self.surf, (self.x-20*len(self.msg), self.y), self.msg, white)
-			else: self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y), self.msg, white)
+			else: self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y-1), self.msg, white)
 		if self.align == 'bottomleft':
 			if self.over: self.button = ft34.render_to(self.surf, (self.x, self.y-45), self.msg, white)
-			else: self.button = ft30.render_to(self.surf, (self.x, self.y-40), self.msg, white)
+			else: self.button = ft30.render_to(self.surf, (self.x, self.y-41), self.msg, white)
 		if self.align == 'bottomright':
 			if self.over: self.button = ft34.render_to(self.surf, (self.x-20*len(self.msg), self.y-45), self.msg, white)
-			else: self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y-40), self.msg, white)
+			else: self.button = ft30.render_to(self.surf, (self.x-18*len(self.msg), self.y-41), self.msg, white)
 
 white = (255,255,255)
 black = (0,0,0)
